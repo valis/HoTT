461cac05614a6032cbe893170eafb0eef7cc33c3
valis <valery.isaev@gmail.com>
2013-03-22 18:27:39 +0400

    Fixed bug with variables renaming

--- Normalization.hs
+++ Normalization.hs
@@ -14,21 +14,24 @@ arr :: Term -> Term -> Term
 arr a b = Pi (Lam "_" a b)
 
 instance Eq Term where
-    Zero == Zero = True
-    Suc == Suc = True
-    Top == Top = True
-    Bot == Bot = True
-    Unit == Unit = True
-    Nat == Nat = True
-    Var x == Var x' = x == x'
-    Refl t == Refl t' = t == t'
-    Cong t == Cong t' = t == t'
-    Id t s == Id t' s' = t == t' && s == s'
-    App t s == App t' s' = t == t' && s == s'
-    Lam x t s == Lam x' t' s' = t == t' && s == s'
-    Pi t == Pi t' = t == t'
-    R t z s n == R t' z' s' n' = t == t' && z == z' && s == s' && n == n'
-    _ == _ = False
+    (==) = eq 0
+      where
+        eq n Zero Zero = True
+        eq n Suc Suc = True
+        eq n Top Top = True
+        eq n Bot Bot = True
+        eq n Unit Unit = True
+        eq n Nat Nat = True
+        eq n (Var x) (Var x') = x == x'
+        eq n (Refl t) (Refl t') = eq n t t'
+        eq n (Cong t) (Cong t') = eq n t t'
+        eq n (Id t s) (Id t' s') = eq n t t' && eq n s s'
+        eq n (App t s) (App t' s') = eq n t t' && eq n s s'
+        eq n (Lam x t s) (Lam x' t' s') = eq n t t' &&
+            eq (n + 1) (rename s x (' ':show n)) (rename s' x' (' ':show n))
+        eq n (Pi t) (Pi t') = eq n t t'
+        eq n (R t z s p) (R t' z' s' p') = eq n t t' && eq n z z' && eq n s s' && eq n p p'
+        eq _ _ _ = False
 
 typeOf :: M.Map String Term -> Term -> Maybe Term
 typeOf m (Var x) = M.lookup x m
@@ -118,17 +121,17 @@ action (d : a) (Tm t) = case (d, typeOf M.empty t) of
 leftMap :: Integer -> Base -> Base
 leftMap n = action (genericReplicate n Ld)
 
-app :: Integer -> Base -> Base -> Base
-app n (Fun _ _ f) x = f n [] x
-app n (Tm t) x = Tm $ t `App` reify n x
+app :: Integer -> Ctx -> Base -> Base -> Base
+app n _ (Fun _ _ f) x = f n [] x
+app n c (Tm t) x = Tm $ t `App` reify n c x
 
-rec :: Integer -> Term -> Base -> Base -> Base -> Base
-rec 0 _ z _ (Tm Zero) = z
-rec 0 t z s (Tm (App Suc p)) = app 0 (app 0 s (Tm p)) (rec 0 t z s (Tm p))
-rec n t z s (Tm (Refl p)) | n > 0 = Tm $ Refl $ reify n (rec (n - 1) t z s (Tm p))
-rec n t z s (Tm e) = Tm $ genCong (Lam "e" Nat $ R t (reify 0 z) (reify 0 s) (Var "e")) n `App` e
+rec :: Integer -> Ctx -> Term -> Base -> Base -> Base -> Base
+rec 0 _ _ z _ (Tm Zero) = z
+rec 0 c t z s (Tm (App Suc p)) = app 0 c (app 0 c s (Tm p)) (rec 0 c t z s (Tm p))
+rec n c t z s (Tm (Refl p)) | n > 0 = Tm $ Refl $ reify n c (rec (n - 1) c t z s (Tm p))
+rec n c t z s (Tm e) = Tm $ genCong (Lam "e" Nat $ R t (reify 0 c z) (reify 0 c s) (Var "e")) n `App` e
     -- TODO: cong-dep
-rec _ _ _ _ _ = error "rec: ERROR"
+rec _ _ _ _ _ _ = error "rec: ERROR"
 
 genRefl' :: Base -> Integer -> Base
 genRefl' (Tm t) n = Tm (genRefl t n)
@@ -143,9 +146,6 @@ genRefl :: Term -> Integer -> Term
 genRefl c n | n < 0 = error "ERROR: genRefl"
 genRefl c 0 = c
 genRefl c n = Refl (genRefl c (n - 1))
-
-chooseName :: String -> M.Map String a -> String
-chooseName x m = if M.member x m then chooseName (x ++ "'") m else x
 
 rename :: Term -> String -> String -> Term
 rename q x r | x == r = q
@@ -183,25 +183,25 @@ eval Suc n _ = Fun "n" Nat $ \k _ t -> Tm (suc k t)
     suc _ _ = error "suc: ERROR"
 eval v@(Var x) n c = maybe (Tm v) (\s -> s) (M.lookup x c)
 eval (Refl t) n c = genRefl' (eval t n c) 1
-eval (App a b) n c = app n (eval a n c) (eval b n c)
-eval (Lam x t r) n c = let x' = chooseName x c in Fun x' (normCtx n c t) $
-    \k m s -> eval (rename r x x') k $ M.insert x' s (M.map (action m) c)
+eval (App a b) n c = app n c (eval a n c) (eval b n c)
+eval (Lam x t r) n c = Fun x (normCtx n c t) $ \k m s -> eval r k $ M.insert x s (M.map (action m) c)
 eval (Id a b) n c = Tm $ genRefl (Id (normCtx n c a) (normCtx n c b)) n
     -- TODO: check if 'a' and 'b' have function type
-eval (Pi (Lam x t r)) n c = let x' = chooseName x c
-    in Tm $ genRefl (Pi $ Lam x' (normCtx n c t) (normCtx n c (rename r x x'))) n
+eval (Pi (Lam x t r)) n c = Tm $ genRefl (Pi $ Lam x (normCtx n c t) (normCtx n c r)) n
 eval (Pi _) _ _ = error "eval: Pi without Lam"
 eval (Cong t) n c = case eval t n c of
     Fun x t' f -> Fun x t' $ \k m -> f (k + 1) (Ud : m)
     _ -> error "eval: ERROR"
-eval (R t z s p) n c = rec n t (leftMap n $ eval z n c) (leftMap n $ eval s n c) (eval p n c)
+eval (R t z s p) n c = rec n c t (leftMap n $ eval z n c) (leftMap n $ eval s n c) (eval p n c)
 
-reify :: Integer -> Base -> Term
-reify _ (Tm t) = t
-reify n (Fun x t f) = Lam x t $ reify n $ f n [] $ Tm (Var x)
+reify :: Integer -> Ctx -> Base -> Term
+reify _ _ (Tm t) = t
+reify n c (Fun x t f) = if M.member x c
+    then reify n c (Fun (x ++ "'") t f)
+    else Lam x t $ reify n (M.insert x (Tm (Var x)) c) $ f n [] $ Tm (Var x)
 
 normCtx :: Integer -> Ctx -> Term -> Term
-normCtx n c t = reify n (eval t n c)
+normCtx n c t = reify n c (eval t n c)
 
 norm :: Term -> Term
 norm = normCtx 0 M.empty
@@ -215,6 +215,7 @@ nat n = Suc `App` nat (n - 1)
 cR t = R (Lam "_" Nat t)
 eqTest1 = Lam "x" Nat $ Lam "y" Nat $ Var "x" `App` Var "y"
 eqTest2 = Lam "x" Nat $ Lam "x" Nat $ Var "x" `App` Var "x"
+eqTest3 = Lam "z" Nat $ Lam "t" Nat $ Var "z" `App` Var "t"
 omega = Lam "x" Nat $ Var "x" `App` Var "x"
 one t = Lam "x" (t `arr` t) $ Lam "y" t $ Var "x" `App` Var "y"
 i t = Lam "x" t (Var "x")
@@ -241,7 +242,8 @@ x ~?/= y = TestCase $ assertBool (show x ++ " shoud not be equal to " ++ show y)
 
 main = fmap (\_ -> ()) $ runTestTT $ test
     $    label "(==)"
-    [ eqTest1 ~?/= eqTest2
+    [ eqTest1 ~?= eqTest3
+    , eqTest1 ~?/= eqTest2
     ] ++ label "alpha conversion"
     [ norm alphaTest ~?= one Nat
     ] ++ label "plus"
